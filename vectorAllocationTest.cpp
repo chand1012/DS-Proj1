27cc585e8127d48228877d8b95815ee05a6eabb5
Chandler Lofland <chandlerl2000@gmail.com>
2021-02-04 15:56:53 -0500

    Fix iteration array

--- vectorAllocationTest.cpp
+++ vectorAllocationTest.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-    unsigned int iterations[8] = {10, 100, 1000, 10000, 100000, 1000000, 1000000, 100000000};
+    unsigned int iterations[8] = {10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
     unsigned int iteration;
 
     const int testint = 2000000;
